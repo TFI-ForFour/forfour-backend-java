8213293e6d8ee4d4c2b657bc14e64174b17bdae5
koreaioi <koreaioi@gachon.ac.kr>
2025-11-06 02:47:36 +0900

    feat: JwtValidator 로직 추가

--- src/main/java/com/forfour/global/jwt/utils/JwtValidator.java
+++ src/main/java/com/forfour/global/jwt/utils/JwtValidator.java
@@ -1,4 +1,46 @@
 package com.forfour.global.jwt.utils;
 
+import com.forfour.global.jwt.exception.*;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.WeakKeyException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+
+@Component
 public class JwtValidator {
+
+    private final Key key;
+
+    public JwtValidator(@Value("${forfour.jwt.secretKey}") String secretKey) {
+        this.key = Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+
+    public void verifyAccessToken(String token) {
+        try{
+            Jwts.parser()
+                    .verifyWith((SecretKey) key)
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+        }catch (ExpiredJwtException e) {
+            throw new CustomExpiredJwtException();
+        } catch (UnsupportedJwtException e) {
+            throw new CustomUnsupportedJwtException();
+        } catch (MalformedJwtException e) {
+            throw new CustomMalformedJwtException();
+        } catch (SignatureException e) {
+            throw new CustomSignatureException();
+        } catch (IllegalArgumentException e) {
+            throw new CustomIllegalArgumentException();
+        } catch (WeakKeyException e) {
+            throw new CustomWeakKeyException();
+        } catch (Exception e) {
+            throw new CustomJwtUnknownException();
+        }
+    }
+
 }
