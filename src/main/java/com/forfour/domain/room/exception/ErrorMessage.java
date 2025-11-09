c93bffc2235d74b883cb37e224fd329b0f53c5da
koreaioi <koreaioi@gachon.ac.kr>
2025-11-09 23:48:18 +0900

    feat: 방 참여 예외 추가

--- src/main/java/com/forfour/domain/room/exception/ErrorMessage.java
+++ src/main/java/com/forfour/domain/room/exception/ErrorMessage.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum ErrorMessage {
 
     ROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "[산책 방]을 찾을 수 없습니다."),
+    ROOM_IS_NOT_RECRUITING(HttpStatus.BAD_REQUEST, "[산책 방]이 현재 모집 중이 아닙니다."),
+    ROOM_IS_FULL(HttpStatus.BAD_REQUEST, "[산책 방]의 회원수가 초과됐습니다."),
     ;
 
     private final HttpStatus status;
