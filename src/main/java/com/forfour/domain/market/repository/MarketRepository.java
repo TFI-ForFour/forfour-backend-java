d3299b01c28bb484cbb39980a5ade7ebd5c99705
koreaioi <koreaioi@gachon.ac.kr>
2025-11-09 18:11:26 +0900

    feat: MarketRepository 추가

---
+++ src/main/java/com/forfour/domain/market/repository/MarketRepository.java
@@ -0,0 +1,9 @@
+package com.forfour.domain.market.repository;
+
+import com.forfour.domain.market.entity.Market;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MarketRepository extends JpaRepository<Market, Long> {
+}
