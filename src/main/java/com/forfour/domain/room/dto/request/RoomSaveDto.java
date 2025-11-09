646a726053464e065f9e701d4a1e37806b999924
koreaioi <koreaioi@gachon.ac.kr>
2025-11-09 22:48:27 +0900

    feat: Dto 스웨거 명세 추가

--- src/main/java/com/forfour/domain/room/dto/request/RoomSaveDto.java
+++ src/main/java/com/forfour/domain/room/dto/request/RoomSaveDto.java
@@ -1,11 +1,25 @@
 package com.forfour.domain.room.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDateTime;
 
 public record RoomSaveDto(
+        @NotBlank
+        @Schema(description = "산책방 제목", example = "테스트 산책방 1")
         String title,
+
+        @NotBlank
+        @Schema(description = "경로 ID", example = "1")
         Long pathId,
+
+        @Schema(description = "미션 명", example = "PLOGGING, DELIVERY, PARK, NO_MISSION")
         String missionName,
+
+        @NotNull
+        @Schema(description = "산책 시간")
         LocalDateTime startAt
 ) {
 }
