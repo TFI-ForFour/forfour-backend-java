bc56d0226692bdebc5d199a2b9fe60ab177d95b1
koreaioi <koreaioi@gachon.ac.kr>
2025-11-24 23:16:30 +0900

    hotfix: 카카오 API 호출 로그 추가

--- src/main/java/com/forfour/global/auth/service/KakaoService.java
+++ src/main/java/com/forfour/global/auth/service/KakaoService.java
@@ -3,8 +3,10 @@ package com.forfour.global.auth.service;
 import com.forfour.global.auth.kakao.dto.KaKaoDTO;
 import com.forfour.global.auth.kakao.utils.KakaoUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class KakaoService {
@@ -18,8 +20,11 @@ public class KakaoService {
     }
 
     private KaKaoDTO.KakaoUserInfoResponse getKakaoUserInfoByAuthCode(String authCode) {
+        log.info("카카오 토큰 조회 API 호출");
         KaKaoDTO.KakaoAccessToken kakaoAccessToken = kakaoUtil.reqeustKakaoToken(authCode);
+        log.info("카카오 토큰으로 유저 정보 조회 API 호출");
         KaKaoDTO.KakaoUserInfoResponse userInfo = kakaoUtil.requestKakaoProfile(kakaoAccessToken.access_token());
+        log.info("유저 정보 조회 API 호출 성공");
         return userInfo;
     }
 
